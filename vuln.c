89179998d99ec96911fef2e9d73e8b15a9ca3a5d
tlb <thomas.lebaron@free.fr>
2018-10-04 11:02:23 -0400

    all codes

---
+++ vuln.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Struct {
+	char buffer[8];
+	int num;
+};
+
+int main(int argc, char **argv){
+
+	char buffer [16];
+	gets(buffer);
+}
