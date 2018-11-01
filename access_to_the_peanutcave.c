e3044d43a9ebac753bfcffac012496bac1726e6d
root <thomas.lebaron@free.fr>
2018-11-01 09:50:35 -0400

    modified the access to the peanutcave for more lore

--- access_to_the_peanutcave.c
+++ access_to_the_peanutcave.c
@@ -5,19 +5,21 @@ int main(){
 	printf("Door> The Peanutcave is protected by the best password generator of the planet\n");
 
 	unsigned int random;
-	random = rand();
 
-	printf("Door> Enter the password:\n");
-	unsigned int key=0;
-	scanf("%d", &key);
+	while(){
+		random = rand();
 
-	if( (key ^ random) == 0xdeadbeef ){
-		printf("Alfred> Good morning master Peanut, it is good to see you\n");
-		execve("/bin/shh");
-		return 0;
+        	printf("Door> Enter the password:\n");
+        	unsigned int key=0;
+        	scanf("%d", &key);
+
+        	if( (key ^ random) == 0xdeadbeef ){
+                	printf("Alfred> Good morning master Peanut, it is good to see you\n");
+                	execve("/bin/shh");
+                	return 0;
+        	}
+
+        	printf("Door> Mouhaha do you think you can fool the Great Door of the Peanutcave? Your failure changed the password, you will never find it!\n");
 	}
-
-	printf("Door> You are not Peanut! I am calling the security\n");
-	return 0;
 }
 
