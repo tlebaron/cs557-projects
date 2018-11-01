e47399ab2c736883f33972bda2545a69463079e5
root <thomas.lebaron@free.fr>
2018-11-01 08:56:38 -0400

    archive of assignment2, first tests for assignment3. seems too easy for now

---
+++ peanut_descriptor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char buf[32];
+
+void wowyoutoo() {
+	printf("Tell us how much you love peanut!");
+	char buffer[64];
+	gets(buffer);
+}
+
+int main(int argc, char* argv[], char* envp[]){
+        if(argc<2){
+                printf("pass argv[1] a number\n");
+                return 0;
+        }
+        int fd = atoi( argv[1] ) - 0x1234;
+        int len = 0;
+        len = read(fd, buf, 32);
+        if(!strcmp("ILOVEPEANUT\n", buf)){
+	    wowyoutoo();
+        }
+        printf("learn about Linux file IO\n");
+        return 0;
+
+}
